c09199aed42df45610f6207b55b81b9af5c41eab
jeanlucaflores-013 <jeanlucaflores@gmail.com>
2026-02-08 17:52:26 -0600

    LAB3

---
+++ LABORATORIO 3/Laboratorio_Act2_Jeanluca_Alarcón/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+string nombre, curso;
+Console.WriteLine("Ingrese su nombre: ");
+nombre = Console.ReadLine();
+Console.WriteLine("Cual es el nombre del curso? ");
+curso = Console.ReadLine();
+Console.WriteLine(nombre + ", tienes que esforzarte más en " + curso);
+Console.WriteLine("Nunca dejes de prácticar");
+Console.WriteLine("Hoy es un buen dia para no ser de industrial");
+Console.WriteLine("Presione una tecla para salir* ");
+Console.ReadKey();
+
+
+
